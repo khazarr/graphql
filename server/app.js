8f6950958ca239cd46060653be602536e3b37b0d
Karol Swieca <swieca.karol@gmail.com>
2018-10-30 21:39:14 +0100

    graphql schema

--- server/app.js
+++ server/app.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const graphqlHTTP = require('express-graphql')
 const PORT = 4000
+const schema = require('./schema/schema')
 
 app.use('/graphql', graphqlHTTP({
-
+  schema
 }));
 
 app.listen(PORT, () => {
